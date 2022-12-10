ed47f6b39a5159e8e333dd4d6cc09453945145eb
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-12-10 20:53:35 +0500

    first commit

--- app.js
+++ app.js
@@ -6,22 +6,26 @@ const homeRouter = require("./routers/homeRouter");
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));  
-app.use('/',homeRouter);
+app.use(express.urlencoded({ extended: false }));
+app.use("/", homeRouter);
 
-mongoose.connect("mongodb+srv://QaziAhmad:qazi03331602121ahmad@cluster0.82zpao1.mongodb.net/loginproject").then(function(){
+mongoose
+  .connect(
+    "mongodb+srv://QaziAhmad:qazi03331602121ahmad@cluster0.82zpao1.mongodb.net/loginproject"
+  )
+  .then(function () {
     console.log("Database connected successfully");
-}).catch(function(err){
+  })
+  .catch(function (err) {
     console.log(err);
     console.log("There is some issue while connecting database");
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
 });
-
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port : ${port}`);
-})
